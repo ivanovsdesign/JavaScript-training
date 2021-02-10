041aaad55ed42dc7c0bee9e7683c8c30913d731d
Oleg Ivanov <78684921+ivanovsdesign@users.noreply.github.com>
2021-02-11 00:28:02 +0300

    Add code according to YouTube lesson

--- 4-navbar/setup/app.js
+++ 4-navbar/setup/app.js
@@ -3,3 +3,14 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle"),
+      links = document.querySelector(".links");
+
+navToggle.addEventListener("click", () => {
+    if (links.classList.contains("show-links")) {
+        links.classList.remove("show-links");
+    } else {
+        links.classList.toggle("show-links");
+    }
+})
