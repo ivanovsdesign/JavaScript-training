d7bf3f3a39a5c776071e28c7a9d8df1b9dd200fb
Oleg Ivanov <78684921+ivanovsdesign@users.noreply.github.com>
2021-02-20 22:52:34 +0300

    Add slider project. Needs fixing button issues

---
+++ 15-slider/setup/app.js
@@ -0,0 +1,43 @@
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+
+slides.forEach(function(slide, index){
+    slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+nextBtn.addEventListener("click", function(){
+    counter++;
+});
+prevBtn.addEventListener("click", function(){
+    counter--;
+});
+
+function carousel(){
+    // //working with slides
+
+    // if (counter === slides.length) {
+    //     counter = 0;
+    // }
+    // if (counter < 0) {
+    //     counter.slides.length - 1;
+    // }
+
+    //workin with buttons
+    if (counter < slides.length -1) {
+        nextBtn.style.display = "block";
+    } else {
+        nextBtn.style.display = "none";
+    }
+    if (counter > slides.length -1) {
+        prevBtn.style.display = "block";
+    } else {
+        prevBtn.style.display = "none";
+    }
+
+    slides.forEach(function(){
+        slide.style.transform = `translateX(-${counter*100})`;
+
+    })
+}
